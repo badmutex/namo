9b54d756f0fcbc60629ede77e20373ab83b9c335
Badi' Abdul-Wahid <abdulwahidc@gmail.com>
2014-12-01 16:02:20 -0500

    learn to install zfs

---
+++ install_zfs.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -e
+set -x
+
+### Make sure you run yum update -y && reboot before this to get the right kernel headers and kernel
+
+yum localinstall --nogpgcheck \
+    https://download.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-2.noarch.rpm \
+    http://archive.zfsonlinux.org/epel/zfs-release.el7.noarch.rpm
+yum install -y kernel-devel zfs
