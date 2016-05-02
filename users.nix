0fb8882b56ae975e5de16c7c6c323508c62753bc
Badi' Abdul-Wahid <abdulwahidc@gmail.com>
2016-05-01 22:39:44 -0400

    use mutable users

--- users.nix
+++ users.nix
@@ -4,14 +4,12 @@
 
 {
 
-  users.mutableUsers = false;
-  users.users.root.passwordFile = "/etc/nixos/users/root/password";
+  users.mutableUsers = true;
   users.extraGroups.mandos.gid = 1002;
   users.extraUsers.badi = {
     isNormalUser = true;
     uid = 1001;
-    extraGroups = [ "wheel" "mandos" ];
-    passwordFile = "/etc/nixos/users/badi/password";
+    extraGroups = [ "wheel" "mandos" "networkmanager" ];
     openssh.authorizedKeys.keys = [
       "ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEA0AiBoBgua3YZTBfOgQk5JLdqzoY7e9ywRU3481mV2W9PRAJpkwTJ9l2vgfMt4Pj5xLJFzhwYRlZ607blXt/3pmxjfDHhsi00iXmxpkY3OLb0Fqpzeia0ibezO+7unHjb7/EhvQHX7ZBLGSsWQxDTwRhtTQF1SSrUqej1JJ+IfT6t7o+VZMIiPPswRa1pDCymI/gk9sW2RGDZr0CpTecgAEF+94Bfu6lckYLSJzIyqGzC650oqA21ubrmN7XkbgIY3jrCl13DAOArRsSlSsRMwsp8JbrJXShLnTDNxGNY/9FvEBDr3VDuDCa/lyTwdoyqn1Ev0wwy3EiQLSNdSEJ3dw== abdulwahidc@gmail.com"
     ];
